26505fa5a515dd02675423c1611c004e33aab987
Sewar-web <s.maqableh@yahoo.com>
2021-02-09 14:30:41 +0200

    function code

---
+++ lab06/code.js
@@ -0,0 +1,38 @@
+function animal()
+{
+    var animals =prompt('whate do you want from animlas');
+
+
+while( animals !== "cat"  &&  animals !== "dog")
+
+{
+
+    animals=prompt("whate do you want from animlas");
+
+}
+
+var typeOFanimals='';
+
+if(animals == "cat")
+{
+    typeOFanimals = '<img src ="images/cat.jpg"/>';
+}
+
+else if (animals == "dog")
+{
+    typeOFanimals = '<img src ="images/dog.jpg"/>';
+}
+var numofanimals=prompt('whats the number of animals');
+var result='';
+
+for(var i=0 ; i < numofanimals ; i++)
+{
+    result =result +typeOFanimals;
+}
+
+document.write(result);
+
+}
+
+
+animal();
